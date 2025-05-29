73a2f727dfe3c3f267f550eaa5af50746a201084
agarciapu <64229084+agarciapu@users.noreply.github.com>
2025-05-29 12:00:15 -0500

    lab03 y lab04

---
+++ LABORATORIO04/Assets/Scripts/Cup.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+
+public class Cup : MonoBehaviour
+{
+     void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Colisión detectada con: " + collision.gameObject.name);
+
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            Debug.Log("+1");
+            GameManager.Instance.AddScore(1);
+        }
+    }
+	
+}
---
+++ LABORATORIO04/Assets/Scripts/GameManager.cs
@@ -0,0 +1,100 @@
+﻿
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+	#region Singleton class: GameManager
+
+	public static GameManager Instance;
+
+	public TextMeshProUGUI scoreText;
+    int score = 0;
+
+	int puntos = 0;
+
+	void Awake ()
+	{
+		if (Instance == null) {
+			Instance = this;
+		}
+	}
+
+	#endregion
+
+	Camera cam;
+
+	public Ball ball;
+	public Trajectory trajectory;
+	[SerializeField] float pushForce = 4f;
+
+	bool isDragging = false;
+
+	Vector2 startPoint;
+	Vector2 endPoint;
+	Vector2 direction;
+	Vector2 force;
+	float distance;
+
+	//---------------------------------------
+	void Start ()
+	{
+		cam = Camera.main;
+		ball.DesactivateRb ();
+	}
+
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown (0)) {
+			isDragging = true;
+			OnDragStart ();
+		}
+		if (Input.GetMouseButtonUp (0)) {
+			isDragging = false;
+			OnDragEnd ();
+		}
+
+		if (isDragging) {
+			OnDrag ();
+		}
+	}
+
+	//-Drag--------------------------------------
+	void OnDragStart ()
+	{
+		ball.DesactivateRb ();
+		startPoint = cam.ScreenToWorldPoint (Input.mousePosition);
+
+		trajectory.Show ();
+	}
+	public void AddScore(int value)
+    {
+        score += value;
+        scoreText.text = "Puntos: " + score;
+    }
+
+	void OnDrag ()
+	{
+		endPoint = cam.ScreenToWorldPoint (Input.mousePosition);
+		distance = Vector2.Distance (startPoint, endPoint);
+		direction = (startPoint - endPoint).normalized;
+		force = direction * distance * pushForce;
+
+		//just for debug
+		Debug.DrawLine (startPoint, endPoint);
+
+
+		trajectory.UpdateDots (ball.pos, force);
+	}
+
+	void OnDragEnd ()
+	{
+		//push the ball
+		ball.ActivateRb ();
+
+		ball.Push (force);
+
+		trajectory.Hide ();
+	}
+
+}
